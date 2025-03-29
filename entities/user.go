e146c830dcd1b882a5d1686036064f4b69fc54aa
Songwaad <Songw44d@gmail.com>
2025-03-29 16:16:47 +0700

    refactor: rename DeleteAt to DeletedAt and remove json tag

--- entities/user.go
+++ entities/user.go
@@ -5,10 +5,10 @@ import (
 )
 
 type User struct {
-	UserID    string     `json:"user_id" gorm:"primaryKey"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at" gorm:"index"`
+	UserID    string `json:"user_id" gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `gorm:"index"`
 
 	Email        string `gorm:"unique"`
 	Password     string
