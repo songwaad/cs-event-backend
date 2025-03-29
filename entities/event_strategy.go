fc46b32e4daafa29737a3002973d98f21c2a1b51
Songwaad <Songw44d@gmail.com>
2025-03-29 20:19:52 +0700

    refactor: rename ID to EventStrategyID

--- entities/event_strategy.go
+++ entities/event_strategy.go
@@ -1,9 +1,9 @@
 package entities
 
 type EventStrategy struct {
-	ID         uint `json:"id" gorm:"primaryKey"`
-	StrategyID uint
-	Strategy   Strategy
-	Goal       string `json:"goal"`
-	Tactic     string `json:"tactic"`
+	EventStrategyID uint `json:"event_strategy_id" gorm:"primaryKey"`
+	StrategyID      uint `json:"stragy_id"`
+	Strategy        Strategy
+	Goal            string `json:"goal"`
+	Tactic          string `json:"tactic"`
 }
