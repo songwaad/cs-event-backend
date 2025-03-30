dc729998b69e3189abf104baf7eff5e3361320ca
Songwaad <Songw44d@gmail.com>
2025-03-31 01:06:33 +0700

    feat: add function to create notification in usecase

--- usecases/notification_usecase.go
+++ usecases/notification_usecase.go
@@ -3,6 +3,7 @@ package usecases
 import "github.com/songwaad/cs-event-backend/entities"
 
 type NotificationUsecase interface {
+	CreateNotify(notify *entities.Notification) error
 	GetNotifyByUserID(userID string) ([]entities.Notification, error)
 	InActive(id uint) error
 	DeleteNotify(id uint) error
@@ -14,6 +15,10 @@ type NotificationService struct {
 
 func NewNotificationService(repo NotificationRepo) *NotificationService {
 	return &NotificationService{repo: repo}
+}
+
+func (s *NotificationService) CreateNotify(notify *entities.Notification) error {
+	return s.repo.CreateNotify(notify)
 }
 
 func (s *NotificationService) GetNotifyByUserID(userID string) ([]entities.Notification, error) {
